f2b68ec1a479d4e2da659ab4619a97dd31b4bcc0
Chong Wang <cwang@dminc.com>
2019-05-22 18:44:42 -0400

    init tomcat-cicd project

---
+++ src/main/java/com/cwinc/aws/HelloController.java
@@ -0,0 +1,17 @@
+package com.cwinc.aws;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by cwang on 5/20/2019.
+ */
+@RequestMapping("/hello")
+@RestController
+public class HelloController {
+    @GetMapping
+    public String hello(){
+        return  "Hello Youtube";
+    }
+}
