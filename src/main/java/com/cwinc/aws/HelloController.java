35d5fd7f145ce239b47a922eb66d7a2bc9d8c83e
cxw0712 <cxw0712@gmail.com>
2019-06-15 23:39:00 -0400

    Change back to Chong Wang

--- src/main/java/com/cwinc/aws/HelloController.java
+++ src/main/java/com/cwinc/aws/HelloController.java
@@ -12,6 +12,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping
     public String hello(){
-        return  "Hello Chong W!";
+        return  "Hello Chong Wang!";
     }
 }
